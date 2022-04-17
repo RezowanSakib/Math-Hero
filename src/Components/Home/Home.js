80ddd4095527b433afbd5eb089c39cd635e633df
Sakib Md Rezowan <sakibmdrezowan@gmail.com>
2022-04-17 11:42:02 +0600

    slider added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Courses from "../Courses/Courses";
+import Slider from "../Slider/Slider";
+
+const Home = () => {
+  return (
+    <div>
+      <Slider></Slider>
+      <Courses></Courses>
+    </div>
+  );
+};
+
+export default Home;
