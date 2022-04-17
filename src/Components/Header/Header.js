af0dea4ff173a522291a425933fbc0211d110f56
Sakib Md Rezowan <sakibmdrezowan@gmail.com>
2022-04-17 22:29:13 +0600

    login complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,8 +2,15 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../../images/mathLogo.jpg";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from "../../firebase.init";
+import { signOut } from "firebase/auth";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+  const handleSignOut = () => {
+    signOut(auth);
+  };
   return (
     <div className="App">
       <Navbar
@@ -33,14 +40,25 @@ const Header = () => {
                 About
               </Nav.Link>
             </Nav>
-            <Nav>
-              <Nav.Link as={Link} to="/login">
-                Login
+
+            {user ? (
+              <Nav.Link onClick={handleSignOut}>
+              Sign Out
+            </Nav.Link>
+            ) : (
+              <Nav>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link>
+              </Nav>
+            )}
+            {/* <Nav.Link onClick={handleSignOut}>
+                Sign Out
               </Nav.Link>
-              <Nav.Link as={Link} to="/register">
-                Register
-              </Nav.Link>
-            </Nav>
+               */}
           </Navbar.Collapse>
         </Container>
       </Navbar>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 const Login = () => {
+ 
+const [signInWithEmailAndPassword, user, loading, error] =
+  useSignInWithEmailAndPassword(auth);
+const emailRef = useRef("");
+const passRef = useRef("");
+const navigate = useNavigate();
+const handleLogin = (event) => {
+  event.preventDefault();
+  const email = emailRef.current.value;
+  const password = passRef.current.value;
+  signInWithEmailAndPassword(email, password);
+};
+const navigateRegister = (event) => {
+  navigate("/register");
+};
+if (user) {
+  navigate("/");
+}
   return (
     <div className="container">
-    
       <div className="row">
-        <Form className="col-md-4">
+        <Form className="col-md-4" onSubmit={handleLogin}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" required />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              required
+              ref={emailRef}
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -17,16 +41,24 @@ const Login = () => {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" required />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              required
+             ref={passRef}
+            />
           </Form.Group>
-        
+
           <Button variant="primary" type="submit">
             Login
           </Button>
-          <p>New to MathHero?<Link to='/register'>Register</Link></p>
+          <p>
+            New to MathHero?
+            <Link to="/register" onClick={navigateRegister}>
+              Register
+            </Link>
+          </p>
         </Form>
-       
-        
       </div>
     </div>
   );
